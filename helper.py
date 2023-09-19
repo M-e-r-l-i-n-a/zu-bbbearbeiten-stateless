db2a17fa6f4824a838d58096289732f63702b1eb
Elena Tafelski <e.tafelski.inf21@stud.bbbaden.ch>
2023-09-19 12:33:36 +0200

    CSV herunterladen. #3

--- helper.py
+++ helper.py
@@ -1,7 +1,9 @@
-from dataclasses import dataclass
 import datetime
+import operator
+from dataclasses import dataclass
+import io
+import csv
 
-# Daten speichern
 items = []
 
 
@@ -9,14 +11,48 @@ items = []
 class Item:
     text: str
     date: datetime
+    category: str
+    description: str
     isCompleted: bool = False
 
 
-# Ver-BBB-isierung
-def add(text, date):
-    text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    date_object = datetime.datetime.strptime(date, "%Y-%m-%d")
-    items.append(Item(text, date_object))
+def get_csv():
+    output = io.StringIO()
+    writer = csv.writer(output)
+    writer.writerow(["Titel", "Datum", "Kategorie", "Beschreibung", "Erledigt?"])
+    for item in items:
+        writer.writerow(
+            [
+                item.text,
+                item.date.strftime("%d.%m.%Y"),
+                item.category,
+                item.description,
+                "x" if item.isCompleted else "o",
+            ]
+        )
+    return output.getvalue()
+
+
+def oneWeekFromToday():
+    today = datetime.datetime.now()
+    oneWeek = datetime.timedelta(weeks=1)
+    return today + oneWeek
+
+
+def add(text, date=None, category=None, description=None):
+    text = text.replace("b", "bbb").replace("B", "Bbb")
+    if date is None:
+        date = oneWeekFromToday()
+    else:
+        date = datetime.datetime.strptime(date, "%Y-%m-%d")
+
+    if category is None:
+        category = "default"
+
+    if description is None:
+        description = ""
+    items.append(Item(text, date, category, description))
+    items.sort(key=lambda x: (x.date, x.category))
 
 
 def get_all():
